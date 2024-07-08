0d777e47655b134c1cf1357b74b1cefa5e39e282
akhrorwork <akhrorwork@gmail.com>
2024-07-08 15:49:10 +0500

    bug fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: [],
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
